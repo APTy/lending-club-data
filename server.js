9fff12a837485e53ac87d238fbea31b143055cd8
R. Tyler Julian <trjulian@gmail.com>
2015-02-26 14:48:51 -0800

    Pull data from LendingClub API

---
+++ server.js
@@ -0,0 +1,43 @@
+var request = require("request");
+var fs = require("fs");
+var express = require('express');
+var app = express();
+
+// Send all requests to the index page
+app.get('/*',function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Load loan data for most recent batch of loans
+request({
+  uri: "https://api.lendingclub.com/api/investor/v1/loans/listing",
+  method: "GET",
+  timeout: 10000,
+  'content-type': 'application/json',
+  headers: {
+    'Authorization': '1DhQpYKsc/JBKZ/OmAHRPaYSgUo='
+  },
+  data: '{showAll: false}'
+}, function(err, res, body) {
+  var data = JSON.parse(body);
+  console.log(data.loans.length);
+}); //.pipe(fs.createWriteStream('test.txt'));
+
+
+// Listen for requests
+var server = app.listen(3000, function() {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
+/*
+curl -v -X GET \
+-H "Content-type: application/json" \
+-H "Authorization: 1DhQpYKsc/JBKZ/OmAHRPaYSgUo=" \
+-d "{showAll: false}" https://api.lendingclub.com/api/investor/v1/loans/listing
+*/
